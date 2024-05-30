d1e45b0815059e6fe398d75df6eb631239c6a335
satyamkodale <satyamkodale@gmail.com>
2024-05-30 22:26:57 +0530

    understood file structure and code base and converted into functional components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </>
   );
 }
--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -11,6 +11,8 @@ import githubDark from "../../assets/github-dark.svg";
 
 import twitterLight from "../../assets/twitter-light.svg";
 import twitterDark from "../../assets/twitter-dark.svg";
+import heroImgWithLaptop from "../../assets/hero with laptop.png";
+import heroImgWithGlasses from "../../assets/hero with glasses.png";
 
 import CV from "../../assets/cv.pdf";
 import { useTheme } from "../../common/ThemeContext";
@@ -22,6 +24,7 @@ export default function Hero() {
   const twitterIcon = theme === "light" ? twitterLight : twitterDark;
   const githubIcon = theme === "light" ? githubLight : githubDark;
   const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+  const heroImg = theme === "light" ? heroImgWithGlasses : heroImgWithLaptop;
 
   console.log(themeIcon, twitterIcon, githubIcon, linkedinIcon, theme);
 
@@ -47,25 +50,26 @@ export default function Hero() {
           Kodale
         </h1>
         <h2>Full Stack Developer</h2>
+
+        <span>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+          With a passion for developing modern React web apps for commercial
+          businesses.
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
       </div>
-      <span>
-        <a href="https://twitter.com/" target="_blank">
-          <img src={twitterIcon} alt="Twitter icon" />
-        </a>
-        <a href="https://github.com/" target="_blank">
-          <img src={githubIcon} alt="Github icon" />
-        </a>
-        <a href="https://linkedin.com/" target="_blank">
-          <img src={linkedinIcon} alt="Linkedin icon" />
-        </a>
-      </span>
-      <p className={styles.description}>
-        With a passion for developing modern React web apps for commercial
-        businesses.
-      </p>
-      <a href={CV} download>
-        <button className="hover">Resume</button>
-      </a>
     </section>
   );
 }
